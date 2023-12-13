0846211681d7111bb3c3b3a6e03904948c0b6b7a
HyperMonkey43 <84732999+HyperMonkey43@users.noreply.github.com>
2023-12-13 19:00:41 +0100

    Main page + Party request UI

---
+++ student_houses_app/student_houses_app/Form2.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace student_houses_app
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            groupBox1 = new GroupBox();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            button1 = new Button();
+            button2 = new Button();
+            label1 = new Label();
+            groupBox1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(label1);
+            groupBox1.Controls.Add(button2);
+            groupBox1.Controls.Add(button1);
+            groupBox1.Controls.Add(textBox2);
+            groupBox1.Controls.Add(textBox1);
+            groupBox1.Location = new Point(26, 26);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(746, 391);
+            groupBox1.TabIndex = 0;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "groupBox1";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(147, 81);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(420, 27);
+            textBox1.TabIndex = 0;
+            textBox1.Text = "Name:";
+            textBox1.TextChanged += textBox1_TextChanged;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(108, 125);
+            textBox2.Multiline = true;
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(498, 165);
+            textBox2.TabIndex = 1;
+            textBox2.Text = "Reason:";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(108, 311);
+            button1.Name = "button1";
+            button1.Size = new Size(131, 44);
+            button1.TabIndex = 2;
+            button1.Text = "Back";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(475, 311);
+            button2.Name = "button2";
+            button2.Size = new Size(131, 44);
+            button2.TabIndex = 3;
+            button2.Text = "Send";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 20F, FontStyle.Regular, GraphicsUnit.Point);
+            label1.Location = new Point(236, 23);
+            label1.Name = "label1";
+            label1.Size = new Size(217, 46);
+            label1.TabIndex = 4;
+            label1.Text = "Party request";
+            // 
+            // Form2
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(groupBox1);
+            Name = "Form2";
+            Text = "Form2";
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private GroupBox groupBox1;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private Button button1;
+        private Button button2;
+        private Label label1;
+    }
+}
