1ce00c7aa2217eeb1fa1ad661deab3991404446c
catalin181 <catalin.irimia14@yahoo.com>
2023-12-13 11:04:07 +0100

    classes

---
+++ student_houses_app/student_houses_app/models/StudentManager.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace student_houses_app.models
+{
+    public class StudentManager
+    {
+        public List<Student> students = new List<Student>;
+
+        public void addStudent (Student student)
+        {
+            int verify = 1;
+
+                foreach (Student s in this.students)
+                    if (s == student) verify = 0;
+
+            if (verify == 1) this.students.Add(student);
+            else MessageBox.Show("Already added this student");
+
+        }
+
+    }
+}
